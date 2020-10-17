38b19136633b9b6e6909a8c014b281e271ce2ddd
vr3n <vickyspatel@gmail.com>
2020-10-17 22:42:37 +0530

    Django Secret key hidden

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .secret_keys import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_9sb6l%de=xs0ieek-hp)uuqywuw1i7#8+h@+=ft)84_se#5o#'
+SECRET_KEY = keys['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
