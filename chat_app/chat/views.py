fd67d71521ee503f3c31bb5b95cbac03b0236c3c
vr3n <vickyspatel@gmail.com>
2020-10-18 01:26:36 +0530

    Styled the Template and fetch the last messages in chat, save the messages in database

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
+from .models import Message
 import json
 
 # Create your views here.
@@ -9,8 +11,9 @@ def index(request):
     return render(request, 'chat/index.html', context)
 
 
+@login_required
 def room(request, room_name):
-
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
     })
