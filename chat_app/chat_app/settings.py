09504d1669cfe03b2834f05f66917755fa3ebfbf
vr3n <vickyspatel@gmail.com>
2020-10-17 23:52:57 +0530

    Created Asyc methods in consumers and Webserver for chat is implemented successfully!

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chat_app.wsgi.application'
 ASGI_APPLICATION = 'chat_app.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
